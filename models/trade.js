3c0983e68136b821e154861932a88d91ce002988
SharaaRe <norouzi.sharare@gmail.com>
2022-11-09 11:40:43 -0500

    add mongoose functionality to app and edit

--- models/trade.js
+++ models/trade.js
@@ -9,8 +9,8 @@ const tradeSchema = new Schema({
     title: {type: String, required: [true, 'title is required']},
     discription: {type: String, required: [true, 'Discription is required']},
     stat: {type: String, enum: ['sold', 'notSold'], default: 'notSold'},
-    image: {type: String, default: 'image_not_available.png'},
-    category: {type: String}, required: ['category is required'], enum: ['Anime', 'Manga', 'Accessories', 'Action Figures']
+    category: {type: String, required:[true, 'category is required'], enum: ['Anime', 'Manga', 'Accessories', 'Action Figures']},
+    image: {type: String, default: 'image_not_available.png'}
 },
 {timeseries: true}
 );
