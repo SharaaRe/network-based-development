8b0ab187f4bcc27fae24e2e475361610cc4c8156
SharaaRe <norouzi.sharare@gmail.com>
2022-12-08 15:35:44 -0500

    milestone4

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //require modules
 const express = require('express');
 const morgan = require('morgan');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const tradeRoutes = require('./routes/tradeRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 const mongoose = require('mongoose')
 
 // create app
@@ -21,9 +26,28 @@ app.listen(port, host, ()=>{
     console.log('Server is running on port', port);
 })
 })
-.catch(err=>console.log(err.message))
+.catch(err=>console.log(err.message));
 
 //mount middleware
+
+app.use(
+    session({
+        secret: "ajfeirf90aeu9eroejfoefj",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/demos'}),
+        cookie: {maxAge: 60*60*1000}
+        })
+);
+app.use(flash());
+app.use((req, res, next) => {
+    //console.log(req.session);
+    res.locals.user = req.session.user || null;
+    res.locals.errorMessages = req.flash('error');
+    res.locals.successMessages = req.flash('success');
+    next();
+});
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('tiny'));
@@ -35,6 +59,9 @@ app.get('/', (req, res)=>{
 });
 
 app.use('/trades', tradeRoutes);
+
+app.use('/users', userRoutes);
+
 app.use((req, res, next) => {
     let err = new Error('The Server cannot locate '+ req.url);
     err.status = 404;
